ea5506c38496c99808ca09f79068e6652cf4bc19
vizarto2024 <152520134+vizarto2024@users.noreply.github.com>
2026-08-04 23:16:38 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/vizarto-emergent">
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/jobs" element={<JobsPage />} />
